9704b5ac54d29f6c0238e4bc5ca70172194070f1
macbookpro <macbookpro@macbookpros-mbp.home>
2017-10-10 15:00:00 +0100

    Initial Commit

---
+++ Gitxcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gitxcode
+//
+//  Created by macbookpro on 10/10/2017.
+//  Copyright (c) 2017 macbookpro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
